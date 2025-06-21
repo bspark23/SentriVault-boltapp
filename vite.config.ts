b1af4947487a877f628e5ebb9bfb970c46ba7794
AdaomaB <mbatab14@gmail.com>
2025-06-21 17:57:25 +0100

    Add dist folder and fix base config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // ✅ THIS FIXES BLANK SCREEN ISSUE ON NETLIFY
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
